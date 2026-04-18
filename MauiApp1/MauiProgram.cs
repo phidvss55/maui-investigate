6df60875b7053439e8365a2db0e58ac9adf1a24c
Phi Dinh <phidv9855@gmail.com>
2026-04-19 00:24:34 +0700

    Enhance monkey details page and improve connectivity handling
    - (feat) implement detailed view for monkey information with image and map button
    - (feat) add refreshing functionality to the main page
    - (feat) integrate geolocation services for finding closest monkey
    - (fix) handle connectivity checks before fetching monkey data
    - (style) update styles for buttons and backgrounds

--- MauiApp1/MauiProgram.cs
+++ MauiApp1/MauiProgram.cs
@@ -16,13 +16,15 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
+		
+		builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
+		builder.Services.AddSingleton<IGeolocation>(Geolocation.Default);
+		builder.Services.AddSingleton<IMap>(Map.Default);
 
 		builder.Services.AddSingleton<MonkeyService>();
 		builder.Services.AddSingleton<MainPage>();
 		builder.Services.AddSingleton<MonkeyViewModel>();
 
-		builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
-		
 		builder.Services.AddSingleton<FirstPage>();
 		builder.Services.AddSingleton<FirstViewModel>();
 		
--- MauiApp1/ViewModel/MonkeyViewModel.cs
+++ MauiApp1/ViewModel/MonkeyViewModel.cs
@@ -1,5 +1,6 @@
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MauiApp1.Model;
 using MauiApp1.Services;
@@ -11,10 +12,17 @@ public partial class MonkeyViewModel : BaseViewModel
     MonkeyService monkeyService;
     public ObservableCollection<Monkey> Monkeys { get; } = new ObservableCollection<Monkey>();
 
-    public MonkeyViewModel(MonkeyService monkeyService)
+    [ObservableProperty] 
+    public partial bool IsRefreshing { get; set; }
+
+    IConnectivity connectivity;
+    IGeolocation _geolocation;
+    public MonkeyViewModel(MonkeyService monkeyService, IConnectivity connectivity, IGeolocation geolocation)
     {
-        Title = "Monkey";
+        Title = "Monkey Finder";
         this.monkeyService = monkeyService;
+        this.connectivity = connectivity;
+        this._geolocation = geolocation;
     }
 
     [RelayCommand]
@@ -32,10 +40,17 @@ public partial class MonkeyViewModel : BaseViewModel
     [RelayCommand]
     async Task GetMonkeys()
     {
+
+        IsRefreshing = true;
         if (IsBusy) return;
 
         try
         {
+            if (connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                await Shell.Current.DisplayAlertAsync("No connectivity", "Please check your internet connection and try again.", "OK");
+                return;
+            }
             IsBusy = true;
             var monkeys = await monkeyService.GetMonkeys();
             if (Monkeys.Count != 0)
@@ -50,7 +65,45 @@ public partial class MonkeyViewModel : BaseViewModel
         }
         finally
         {
+            IsRefreshing = false;
             IsBusy = false;
+        }
+    }
+
+    [RelayCommand]
+    async Task GetClosestMonkey()
+    {
+        if (IsBusy || Monkeys.Count == 0)
+        {
+            await Shell.Current.DisplayAlertAsync("Please wait", "Monkeys are still loading, please wait a moment.", "OK");
+            
+        }
+
+        try
+        {
+            var location = await _geolocation.GetLastKnownLocationAsync();
+            if (location == null)
+            {
+                location = await _geolocation.GetLocationAsync(new GeolocationRequest
+                {
+                    DesiredAccuracy =  GeolocationAccuracy.Medium,
+                    Timeout =  TimeSpan.FromSeconds(10)
+                });
+            }
+            
+            // find closest monkey to us
+            var first = Monkeys.OrderBy(m => location.CalculateDistance(
+                new Location(m.Latitude, m.Longitude), DistanceUnits.Miles
+            )).FirstOrDefault();
+
+            if (first is null) return;
+            
+            await Shell.Current.DisplayAlertAsync("", first.Name + " " + first.Location, "OK");
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine($"Unable to open map: {e.Message}");
+            await Shell.Current.DisplayAlertAsync("Error", e.Message, "OK");
         }
     }
 
